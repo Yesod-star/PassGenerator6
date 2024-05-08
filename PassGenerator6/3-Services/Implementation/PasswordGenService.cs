df70b172212b0d0cb18e7cee5692298d0d16c544
dds <dds@CEPMR3157036>
2024-05-08 17:07:19 -0300

    Alteração de interface

--- PassGenerator6/3-Services/Implementation/PasswordGenService.cs
+++ PassGenerator6/3-Services/Implementation/PasswordGenService.cs
@@ -7,27 +7,27 @@ namespace PassGenerator._3_Services
     {
         public string GenerateRandomPassword(int passLength)
         {
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*?°";
             Random random = new Random();
             char[] password = new char[passLength];
-            char[] password2 = new char[passLength];
-            char[] password3 = new char[passLength];
-            char[] password4 = new char[passLength];
-            char[] password5 = new char[passLength];
+            //char[] password2 = new char[passLength];
+            //char[] password3 = new char[passLength];
+            //char[] password4 = new char[passLength];
+            //char[] password5 = new char[passLength];
             for (int i = 0; i < passLength; i++)
             {
                 password[i] = chars[random.Next(chars.Length)];
-                password2[i] = chars[random.Next(chars.Length)];
-                password3[i] = chars[random.Next(chars.Length)];
-                password4[i] = chars[random.Next(chars.Length)];
-                password5[i] = chars[random.Next(chars.Length)];
+                //password2[i] = chars[random.Next(chars.Length)];
+                //password3[i] = chars[random.Next(chars.Length)];
+                //password4[i] = chars[random.Next(chars.Length)];
+                //password5[i] = chars[random.Next(chars.Length)];
 
             }
-            return new string(password) + "-"
-                + new string(password2) + "-"
-                + new string(password3) + "-"
-                + new string(password4) + "-"
-                + new string(password5);
+            return new string(password);// + "-";
+                //+ new string(password2) + "-"
+                //+ new string(password3) + "-"
+                //+ new string(password4) + "-"
+                //+ new string(password5);
         }
     }
 }
