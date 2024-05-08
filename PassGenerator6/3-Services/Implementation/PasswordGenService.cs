caf5521d763adf98fb2adc95b87373d0565b6ade
pedro <pedro.h.silva18@aluno.senai.br>
2024-05-08 15:45:52 -0300

    gerar 5 senha

--- PassGenerator6/3-Services/Implementation/PasswordGenService.cs
+++ PassGenerator6/3-Services/Implementation/PasswordGenService.cs
@@ -7,12 +7,20 @@ namespace PassGenerator._3_Services
     {
         public string GenerateRandomPassword(int passLength)
         {
-            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789Ç^~@#$?/°";
             Random random = new Random();
             char[] password = new char[passLength];
+            char[] password1 = new char[passLength];
+            char[] password2 = new char[passLength];
+            char[] password3 = new char[passLength];
+            char[] password4 = new char[passLength];
             for (int i = 0; i < passLength; i++)
             {
                 password[i] = chars[random.Next(chars.Length)];
+                password1[i] = chars[random.Next(chars.Length)];
+                password2[i] = chars[random.Next(chars.Length)];
+                password3[i] = chars[random.Next(chars.Length)];
+                password4[i] = chars[random.Next(chars.Length)];
             }
             return new string(password);
         }
