ee6ccd04273d2f4c87dcfb9dcbbec7a488eca3ea
dds <dds@CEPMR3157033>
2024-05-08 15:46:58 -0300

    Gerar 5 senhas se possivel.

--- PassGenerator6/3-Services/Implementation/PasswordGenService.cs
+++ PassGenerator6/3-Services/Implementation/PasswordGenService.cs
@@ -10,8 +10,17 @@ namespace PassGenerator._3_Services
             string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             Random random = new Random();
             char[] password = new char[passLength];
+            char[] password1 = new char[passLength];
+            char[] password2 = new char[passLength];
+            char[] password3 = new char[passLength];
+            char[] password4 = new char[passLength];
+
             for (int i = 0; i < passLength; i++)
             {
+                password[i] = chars[random.Next(chars.Length)];
+                password[i] = chars[random.Next(chars.Length)];
+                password[i] = chars[random.Next(chars.Length)];
+                password[i] = chars[random.Next(chars.Length)];
                 password[i] = chars[random.Next(chars.Length)];
             }
             return new string(password);
